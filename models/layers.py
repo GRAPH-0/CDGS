54d9d27a07ac3bd5e1b5158cded6e3c3d9b3174c
GRAPH-0 <h-huang@buaa.edu.cn>
2023-04-03 22:47:43 +0800

    add time emb

--- models/layers.py
+++ models/layers.py
@@ -30,3 +30,17 @@ def conv1x1(in_planes, out_planes, stride=1, bias=True, dilation=1, padding=0):
                      padding=padding)
     return conv
 
+
+# from DDPM
+def get_timestep_embedding(timesteps, embedding_dim, max_positions=10000):
+    assert len(timesteps.shape) == 1
+    half_dim = embedding_dim // 2
+    # magic number 10000 is from transformers
+    emb = math.log(max_positions) / (half_dim - 1)
+    emb = torch.exp(torch.arange(half_dim, dtype=torch.float32, device=timesteps.device) * -emb)
+    emb = timesteps.float()[:, None] * emb[None, :]
+    emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)
+    if embedding_dim % 2 == 1: # zero pad
+        emb = F.pad(emb, (0, 1), mode='constant')
+    assert emb.shape == (timesteps.shape[0], embedding_dim)
+    return emb
